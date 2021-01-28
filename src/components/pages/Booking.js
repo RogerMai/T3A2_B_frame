b7abd1ba7e1fe998fa369143365532a272c12117
317718 <317718@gmail.com>
2021-01-28 15:15:23 +1000

    Created booking form

--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from '../Form.js'
 
 export default function Booking() {
     return (
         <>
             <h1 className='consulting'>BOOKING</h1>
+            <Form />
         </>
     );
 }

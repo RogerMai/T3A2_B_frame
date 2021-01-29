7b75e08f05dea0b114bb61cb15c7469a6515a000
Roger Mai <cab022010@coderacademy.edu.au>
2021-01-29 23:07:50 +1000

    index-fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import Dropdown from './Dropdown';
+
 
 // function Navbar() {
 //     const [click, setClick] = useState(false)

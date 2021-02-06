c05d8c8928fa5609f1e7e13bbc45a289690428c1
Roger Mai <cab022010@coderacademy.edu.au>
2021-02-07 04:07:22 +1000

    roger-boostrap#4

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Contact.css';
 import API from '../../api';
 import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
+import { Form, Col, Container, Row, Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button } from 'bootstrap';
 
 class Contact extends React.Component {
     state = {
@@ -24,23 +28,54 @@ class Contact extends React.Component {
         return <Spinner />
     } else {
     return (
-        <>
-        <h1 className="contactUs">Contact Us</h1>
-            <div>Contact Number: 07 3805 7265</div>
-            <div>Email: Larry@larryslawncare.com.au</div>
-            <div>
-                <a href="https://www.facebook.com/" target="_blank" className="fab fa-facebook-square fa-3x"> </a>
-                <a href="https://www.instagram.com/?hl=en" target="_blank" className="fab fa-instagram-square fa-3x"> </a>
+        <div className="contact-bg">
+            <div className="contactCard">
+                <div className="contactContainer">
+                    <Container>
+                        <Form>
+                            <Row>
+                                <Col>
+                                    <Card className="uppart">
+                                        <Card.Header className="contact-h1">
+                                            <h1 className="contactUs">Contact Us</h1>
+                                        </Card.Header>
+                                        <Card.Body className="contactList">
+                                            <ul className="list-group">
+                                                <li className="list-group-item">Contact Number: 07 3805 7265</li>
+                                                <li className="list-group-item">Email: Larry@larryslawncare.com.au</li>
+                                            </ul>
+                                        </Card.Body>
+                                        <Card.Body className="contactIcon">
+                                            <a href="https://www.facebook.com/" target="_blank" className="fab fa-facebook-square fa-3x"> </a>
+                                            <a href="https://www.instagram.com/?hl=en" target="_blank" className="fab fa-instagram-square fa-3x"> </a>
+                                        </Card.Body>
+                                    </Card>
+                                    
+                                    <Card className="downpart">
+                                        <Card.Header className="contact-h1">
+                                            <h3 className="service-area">Servicing these areas...</h3>
+                                        </Card.Header>
+                                        <Card.Text>
+                                            {name.map(suburb => <p key={suburb.id}>{suburb.name + ","}</p>)}
+                                        </Card.Text>
+                                    </Card>
+                                </Col>
+                                <Col>
+                                    <Card className="rightpart" >
+                                        <Card.Img src="images/Contact_Page_Image.jpg" alt="Contact Us" />
+                                        <Card.Body className="contactBButton">
+                                            <Link to='booking'>
+                                                <button id='c_booking'>Booking Now<i className='far fa-play-circle' /></button>
+                                            </Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Container>
+                </div>
             </div>
-
-            <h2>Servicing these areas...</h2>
-            <div>
-                {name.map(suburb => <p key={suburb.id}>{suburb.name + ","}</p>)}
-            </div>
-
-           
-            <img src="./images/Contact_Page_Image.jpg" alt="Contact Us" /> 
-        </>
+        </div>
     )};
   }
 }
